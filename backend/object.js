9e0f6ceae107e330e32b5f9df6442f96866742c6
Preisler25 <preisler.andras@gmail.com>
2023-01-08 23:40:49 +0100

    main

---
+++ backend/object.js
@@ -0,0 +1,9 @@
+class Enemy{
+    constructor(x, y, size, hp, attack){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.hp = hp;
+        this.attack = attack;
+    }
+}
